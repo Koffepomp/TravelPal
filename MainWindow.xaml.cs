70b6b5441dcd1891966fe4f047828121e875d6ce
Koffepomp <Zephs1986@gmail.com>
2022-10-25 05:48:26 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace TravelPal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            // Close Main window and open register window
+            MessageBox.Show("Opening register window...");
+            RegisterWindow registerWindow = new();
+            registerWindow.Show();
+            Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Close Main window and open TravelsWindow
+            MessageBox.Show("Logging in... Please wait...");
+        }
+    }
+}
---
+++ RegisterWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+
+namespace TravelPal
+{
+    /// <summary>
+    /// Interaction logic for RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+            AddCountriesToComboBox();
+        }
+
+        private void AddCountriesToComboBox()
+        {
+            foreach (Enum country in Enum.GetValues(typeof(EuropeanCountries)))
+            {
+                cbRegisterCountry.Items.Add(country);
+            }
+        }
+
+        private void btnRegisterAccount_Click(object sender, RoutedEventArgs e)
+        {
+            // Close RegisterWindow and open MainWindow
+            MessageBox.Show("Account succesfully created. Please login!");
+            MainWindow mainWindow = new();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
